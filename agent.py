72bfab0eb71bd0a0f740a47a5a94dc27e59bd96f
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-04-06 16:51:47 +0300

    init agent class

---
+++ agent.py
@@ -0,0 +1,38 @@
+import random
+from collections import deque
+
+import numpy as np
+import torch
+from snake_game_ai import Direction, Point, SnakeGameAI
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+
+class Agent:
+    def __init__(self) -> None:
+        pass
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+
+def train():
+    pass
+
+
+if __name__ == "__main__":
+    train()
