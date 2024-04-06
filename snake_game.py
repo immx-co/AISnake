210679cfcbda1a7aa5754c0882d465544c0109aa
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-04-06 16:42:04 +0300

    apply isort and added pyproject

--- snake_game.py
+++ snake_game.py
@@ -1,8 +1,8 @@
-import pygame
 import random
-from enum import Enum
 from collections import namedtuple
+from enum import Enum
 
+import pygame
 
 pygame.init()
 font = pygame.font.Font("arial.ttf", 25)
--- snake_game_ai.py
+++ snake_game_ai.py
@@ -1,9 +1,9 @@
-import pygame
 import random
-from enum import Enum
 from collections import namedtuple
-import numpy as np
+from enum import Enum
 
+import numpy as np
+import pygame
 
 pygame.init()
 font = pygame.font.Font("arial.ttf", 25)
